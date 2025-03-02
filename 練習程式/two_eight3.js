bd10fa2f465b938ed6678213298b1d125676293d
tu3b47r <y7bv2j5@gmail.com>
2025-03-02 16:42:09 +0800

    Update two_eight3.js

--- 練習程式/two_eight3.js
+++ 練習程式/two_eight3.js
@@ -1,18 +1,9 @@
-function chang(){  //滑鼠過去時，會切換顏色的函式
-    var m1=document.getElementById('m1');
+function addListItem(listId){
+    let newIt = prompt('輸入新增項目文字')；
+    const l1 = document.getElementById(listId);
 
-    m1.style.color ='blue';
-    //把文字變成藍色
+    const newl1 = document.createElement('li');
 
-    m1.style.backgroundColor='green';
-    //將文字框背景顏色設定綠色
-}
-
-function res(){   //滑鼠離開時，會還原原本顏色的函式
-    var m1=document.getElementById('m1');
-
-    m1.style.color='';
-
-
-    m1.style.backgroundColor='orange';
+    newl1.textContent = newlt;
+    l1.appendChild(newl1);
 }
