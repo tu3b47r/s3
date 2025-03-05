a4f51c846f6e5fbe0333a0e4d8986dd30a6a421d
tu3b47r <y7bv2j5@gmail.com>
2025-03-06 00:06:40 +0800

    Update three_one9.js

--- 練習程式(1)/three_one9.js
+++ 練習程式(1)/three_one9.js
@@ -1,5 +1,11 @@
-var i2 = document.getElementById('b7');
-/*這是用JavaScript寫一個指定id=b7的項目存入
+var book2s = document.getElementById('book2s');
+var book2 = book2s.childNodes;
+/*這是用JavaScript寫一個尋找book2s裡的項目存入
 新增的變數之程式語法*/
 
-document.write('項目:' + i2.textContent);
+for(var h = 0;h < book.length;h++)
+  if(book2.item(h).nodeType === 2)
+  /*這是用JavaScript寫一個當此id=book2項目的class
+  之程式語法*/
+    
+    console.log(book2.item(h).textContent);
