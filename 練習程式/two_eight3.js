bd340bd85a1a9b4b44f396c1524f9bf0f1ba6a1e
tu3b47r <y7bv2j5@gmail.com>
2025-03-05 16:57:46 +0800

    Delete 練習程式 directory

--- 練習程式/two_eight3.js
+++
@@ -1,9 +0,0 @@
-function addListItem(listId){
-    let newIt = prompt('輸入新增項目文字')；
-    const l1 = document.getElementById(listId);
-
-    const newl1 = document.createElement('li');
-
-    newl1.textContent = newlt;
-    l1.appendChild(newl1);
-}
--- 練習程式/two_eight5.js
+++
@@ -1,6 +0,0 @@
-function shRes(){
-    var usNa = document.getElementById('usNa');
-    var usPD = document.getElementById('usPD');
-
-    window.alert(usNa.value + '\n' + usPD.value);
-}
