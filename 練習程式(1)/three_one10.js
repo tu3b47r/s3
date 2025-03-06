a5886ad2babf92c8b157bd20f4b9844d95822e40
tu3b47r <y7bv2j5@gmail.com>
2025-03-07 01:07:32 +0800

    Update three_one10.js

--- 練習程式(1)/three_one10.js
+++ 練習程式(1)/three_one10.js
@@ -1,5 +1,5 @@
-var myf = document.getElementById('myf');
-myf.addEventListener('submit',function(e)){
-  if(!windoww.confirm('確定要交表單嗎？'))
+var myf1 = document.getElementById('myf1');
+myf1.addEventListener('submit',function(e)){
+  if(!window.confirm('確定要交表單嗎？'))
      e.preventDefault()；
 },false);
